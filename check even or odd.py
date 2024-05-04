b4852a50736b985fbc5b65a437ff45dc52ed89dc
Manasi Patil <159524590+patilmanii@users.noreply.github.com>
2024-05-04 09:37:22 +0530

    Add files via upload

---
+++ check even or odd.py
@@ -0,0 +1,27 @@
+#even or odd
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+
+
+
+#2
+
+x = 24
+   
+if x % 24 == 0:
+    print(x,"Is Even Number")
+else:
+    print(x, "Is Odd Number")
+     
+y = 19
+   
+if y % 19 == 0:
+    print(y,"Is Even Number")
+else:
+    print(y, "Is Odd Number")
+
