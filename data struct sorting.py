d62e6a10f6c467353f326cad81f6ff2967ec9f7b
Manasi Patil <159524590+patilmanii@users.noreply.github.com>
2024-05-08 13:00:25 +0530

    Add files via upload

---
+++ data struct sorting.py
@@ -0,0 +1,69 @@
+#bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Example usage:
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("Sorted array using Bubble Sort:", arr)
+
+
+
+#selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+# Example usage:
+arr = [64, 34, 25, 12, 22, 11, 90]
+selection_sort(arr)
+print("Sorted array using Selection Sort:", arr)
+
+
+#insertion sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Example usage:
+arr = [64, 34, 25, 12, 22, 11, 90]
+insertion_sort(arr)
+print("Sorted array using Insertion Sort:", arr)
+
+
+
+#Quick sort
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
+
+def quicksort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quicksort(arr, low, pi-1)
+        quicksort(arr, pi+1, high)
+
+# Example usage:
+arr = [64, 34, 25, 12, 22, 11, 90]
+quicksort(arr, 0, len(arr)-1)
+print("Sorted array using Quicksort:", arr)
